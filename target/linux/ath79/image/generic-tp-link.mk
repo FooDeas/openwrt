f5278c86e15a6e435fb6acc54365813ab869964f
FooDeas <foodeas@aeberlein.de>
2023-11-09 23:15:04 +0100

    add TP-Link TL-WR841N/ND v9 with 8M and 16M flash

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -778,6 +778,26 @@ define Device/tplink_tl-wr810n-v2
 endef
 TARGET_DEVICES += tplink_tl-wr810n-v2
 
+define Device/tplink_tl-wr841-v9-8m
+  $(Device/tplink-8mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR841N/ND
+  DEVICE_VARIANT := v9 (8M)
+  TPLINK_HWID := 0x08410009
+  SUPPORTED_DEVICES += tl-wr841n-v9-8m
+endef
+TARGET_DEVICES += tplink_tl-wr841-v9-8m
+
+define Device/tplink_tl-wr841-v9-16m
+  $(Device/tplink-16mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR841N/ND
+  DEVICE_VARIANT := v9 (16M)
+  TPLINK_HWID := 0x08410009
+  SUPPORTED_DEVICES += tl-wr841n-v9-16m
+endef
+TARGET_DEVICES += tplink_tl-wr841-v9-16m
+
 define Device/tplink_tl-wr841hp-v2
   $(Device/tplink-8mlzma)
   SOC := ar9344
